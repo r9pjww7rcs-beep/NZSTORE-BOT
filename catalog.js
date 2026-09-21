f61a41d2a82ecdb88986bccb04770447dc43e8c5
r9pjww7rcs-beep <r9pjww7rcs@privaterelay.appleid.com>
2026-09-21 22:58:14 +0800

    Create catalog.js

---
+++ catalog.js
@@ -0,0 +1,164 @@
+const catalog = {
+
+streaming: [
+
+{
+nama: "Netflix",
+paket: [
+"1 Bulan Share",
+"1 Bulan Private"
+]
+},
+
+{
+nama: "Mango TV",
+paket: [
+"1 Bulan Share",
+"1 Bulan Private"
+]
+},
+
+{
+nama: "Disney+",
+paket: [
+"1 Bulan Share",
+"1 Bulan Private"
+]
+}
+
+],
+
+
+ai: [
+
+{
+nama: "ChatGPT",
+paket: [
+"Share 7U",
+"Share 10U"
+]
+},
+
+{
+nama: "Gemini AI",
+paket: [
+"Link 12 Bulan"
+]
+},
+
+{
+nama: "Perplexity AI",
+paket: [
+"1 Bulan Share",
+"1 Bulan Private"
+]
+},
+
+{
+nama: "Grok AI",
+paket: [
+"3 Hari Private",
+"7 Hari Private"
+]
+}
+
+],
+
+
+editing: [
+
+{
+nama: "Canva",
+paket: [
+"Member",
+"Edu",
+"Neaa"
+]
+},
+
+{
+nama: "CapCut",
+paket: [
+"Share",
+"Private"
+]
+},
+
+{
+nama: "Alight Motion",
+paket: [
+"Share",
+"Private"
+]
+}
+
+],
+
+
+music: [
+
+{
+nama: "Spotify Premium",
+paket: [
+"1 Bulan"
+]
+},
+
+{
+nama: "Apple Music",
+paket: [
+"1 Bulan"
+]
+}
+
+],
+
+
+kebsos: [
+
+{
+nama: "TikTok Followers",
+paket: [
+"100 Followers",
+"200 Followers",
+"500 Followers",
+"1000 Followers"
+]
+},
+
+{
+nama: "TikTok Likes",
+paket: [
+"100 Likes",
+"500 Likes",
+"1000 Likes"
+]
+}
+
+],
+
+
+lainnya: [
+
+{
+nama: "WINK",
+paket: [
+"1 Hari",
+"14 Hari",
+"1 Bulan"
+],
+keterangan:[
+"ONLY ANDROID",
+"LOGIN GOOGLE/EMAIL",
+"PRIVATE ACCOUNT",
+"1 Bulan refill setelah 14 hari"
+]
+}
+
+]
+
+
+};
+
+
+module.exports = catalog;
