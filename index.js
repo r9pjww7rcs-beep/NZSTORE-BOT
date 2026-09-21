c89db11e4a1070f261f587364a244cd3387f24a6
r9pjww7rcs-beep <r9pjww7rcs@privaterelay.appleid.com>
2026-09-22 00:51:19 +0800

    Update index.js update whatsapp connection

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-const config = require("./config");
+const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
+const pino = require("pino");
+const qrcode = require("qrcode-terminal");
+
 const catalog = require("./catalog");
 const database = require("./database");
 
-console.log("NZSTORE BOT STARTED");
-console.log("Total produk:", Object.keys(catalog).length);
+async function startBot() {
+
+  const { state, saveCreds } = await useMultiFileAuthState("session");
+
+  const sock = makeWASocket({
+    auth: state,
+    logger: pino({ level: "silent" })
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("connection.update", (update) => {
+
+    const { connection, qr } = update;
+
+    if (qr) {
+      qrcode.generate(qr, { small: true });
+      console.log("Scan QR WhatsApp");
+    }
+
+    if (connection === "open") {
+      console.log("NZSTORE BOT CONNECTED");
+      console.log("Kategori:", Object.keys(catalog));
+    }
+
+    if (connection === "close") {
+      console.log("Connection closed");
+      startBot();
+    }
+
+  });
+
+}
+
+startBot();
