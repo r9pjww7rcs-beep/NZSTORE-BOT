dc8afbd739deb9eeb7a6f54a09fb9f8587969a46
r9pjww7rcs-beep <r9pjww7rcs@privaterelay.appleid.com>
2026-09-22 00:42:28 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const config = require("./config");
+const catalog = require("./catalog");
+const database = require("./database");
+
+console.log("NZSTORE BOT STARTED");
+console.log("Total produk:", Object.keys(catalog).length);
